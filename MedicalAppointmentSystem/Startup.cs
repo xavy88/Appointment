33b31ac107a74351f3735e37ddffe40b3bee67bf
Javier Urbina <xavyurbina88@gmail.com>
2022-07-25 15:21:52 -0600

    Implementing Connection String

--- MedicalAppointmentSystem/Startup.cs
+++ MedicalAppointmentSystem/Startup.cs
@@ -1,9 +1,11 @@
+using MedicalAppointmentSystem.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,7 @@ namespace MedicalAppointmentSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
